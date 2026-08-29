9d7cb49d37879e58defd7a1ad4305f1d598e5d5a
Showta Ishizaki <zakinko@snowrabbit.org>
2026-08-29 21:27:30 +0900

    Call the four-argument xattr functions on NetBSD
    The extended attribute calls pick their argument list with
    _ALLBSD_SOURCE, but the extra position and options arguments are macOS's
    alone.  NetBSD ships <sys/xattr.h> with the same signatures linux has:
    
      ssize_t fgetxattr(int, const char *, void *, size_t);
      int     fsetxattr(int, const char *, const void *, size_t, int);
      ssize_t flistxattr(int, char *, size_t);
      int     fremovexattr(int, const char *);
    
    so it belongs on the linux side of the #if, and the macOS spelling
    belongs behind __APPLE__.  FreeBSD and OpenBSD have extattr_* instead of
    either and keep falling through to ENOTSUP, which is what they did
    before -- except that they could not compile to find out.
    
    (cherry picked from commit 5b2cf58b1a9ac54f1e476f63579b1f31c5a8868e)

--- src/java.base/unix/native/libnio/fs/UnixNativeDispatcher.c
+++ src/java.base/unix/native/libnio/fs/UnixNativeDispatcher.c
@@ -1246,9 +1246,9 @@ Java_sun_nio_fs_UnixNativeDispatcher_fgetxattr0(JNIEnv* env, jclass clazz,
     const char* name = jlong_to_ptr(nameAddress);
     void* value = jlong_to_ptr(valueAddress);
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__NetBSD__)
     res = fgetxattr(fd, name, value, valueLen);
-#elif defined(_ALLBSD_SOURCE)
+#elif defined(__APPLE__)
     res = fgetxattr(fd, name, value, valueLen, 0, 0);
 #else
     throwUnixException(env, ENOTSUP);
@@ -1267,9 +1267,9 @@ Java_sun_nio_fs_UnixNativeDispatcher_fsetxattr0(JNIEnv* env, jclass clazz,
     const char* name = jlong_to_ptr(nameAddress);
     void* value = jlong_to_ptr(valueAddress);
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__NetBSD__)
     res = fsetxattr(fd, name, value, valueLen, 0);
-#elif defined(_ALLBSD_SOURCE)
+#elif defined(__APPLE__)
     res = fsetxattr(fd, name, value, valueLen, 0, 0);
 #else
     throwUnixException(env, ENOTSUP);
@@ -1286,9 +1286,9 @@ Java_sun_nio_fs_UnixNativeDispatcher_fremovexattr0(JNIEnv* env, jclass clazz,
     int res = -1;
     const char* name = jlong_to_ptr(nameAddress);
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__NetBSD__)
     res = fremovexattr(fd, name);
-#elif defined(_ALLBSD_SOURCE)
+#elif defined(__APPLE__)
     res = fremovexattr(fd, name, 0);
 #else
     throwUnixException(env, ENOTSUP);
@@ -1305,9 +1305,9 @@ Java_sun_nio_fs_UnixNativeDispatcher_flistxattr(JNIEnv* env, jclass clazz,
     size_t res = -1;
     char* list = jlong_to_ptr(listAddress);
 
-#ifdef __linux__
+#if defined(__linux__) || defined(__NetBSD__)
     res = flistxattr(fd, list, (size_t)size);
-#elif defined(_ALLBSD_SOURCE)
+#elif defined(__APPLE__)
     res = flistxattr(fd, list, (size_t)size, 0);
 #else
     throwUnixException(env, ENOTSUP);
