5e388e181626a7dc8f069894994b05dec1ea9c8b
Showta Ishizaki <zakinko@snowrabbit.org>
2026-08-29 16:24:56 +0900

    Supply ElfDecoder::demangle on the ELF BSDs
    decoder_elf.cpp is compiled everywhere except Windows and macOS, so the
    ELF BSDs get an ElfDecoder -- but os/bsd only ever carried
    decoder_machO.cpp, which defines MachODecoder::demangle.  Nothing
    defines ElfDecoder::demangle there and libjvm.so does not link:
    
      decoder_elf.o:(.data.rel.ro._ZTV10ElfDecoder+0x30): undefined
      reference to `ElfDecoder::demangle(char const*, char*, int)'
    
    Add the implementation next to the Mach-O one, guarded so macOS keeps
    using MachODecoder.  The body is the one os/linux/decoder_linux.cpp
    already uses, without the ElfFile::specifies_noexecstack() that only
    Linux calls.
    
    (cherry picked from commit cd3f99f414791554c9ffc606d033dc905c6657ef)

---
+++ src/hotspot/os/bsd/decoder_bsd.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+// macOS decodes Mach-O and brings its own MachODecoder; ElfDecoder is
+// compiled on the other BSDs, where nothing supplied demangle().  This is
+// the same implementation os/linux uses.
+#ifndef __APPLE__
+
+#include "jvm.h"
+#include "utilities/decoder_elf.hpp"
+#include "utilities/globalDefinitions.hpp"
+#include "utilities/permitForbiddenFunctions.hpp"
+
+#include <cxxabi.h>
+
+bool ElfDecoder::demangle(const char* symbol, char *buf, int buflen) {
+  int   status;
+  char* result;
+
+#ifdef PPC64
+  // On PPC64 ElfDecoder::decode() may return a dot (.) prefixed name
+  // (see elfFuncDescTable.hpp for details)
+  if (symbol && *symbol == '.') symbol += 1;
+#endif
+
+  // Don't pass buf to __cxa_demangle. In case of the 'buf' is too small,
+  // __cxa_demangle will call system "realloc" for additional memory, which
+  // may use different malloc/realloc mechanism that allocates 'buf'.
+  if ((result = abi::__cxa_demangle(symbol, nullptr, nullptr, &status)) != nullptr) {
+    jio_snprintf(buf, buflen, "%s", result);
+    // call c library's free
+    permit_forbidden_function::free(result);
+    return true;
+  }
+  return false;
+}
+
+#endif // !__APPLE__
